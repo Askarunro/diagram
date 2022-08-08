6674a820c98701e4f8451d7142676f7c349e7416
Askarunro <paskar.y.a@gmail.com>
2022-08-09 00:46:36 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
-const refs={
-    balanceList: document.querySelector('.balance__list'),
-}
-refs.balanceList.addEventListener('click', e=>{
-    
-    if(e.target.classList.contains('balance__caption')){
-        e.target.classList.toggle('active')
-        e.target.nextElementSibling.classList.toggle('visible')
-    }
-})
+const refs = {
+  balanceList: document.querySelector(".balance__list"),
+};
+refs.balanceList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("balance__caption") && e.target.classList.contains("active")) {
+    e.target.classList.toggle("active");
+    e.target.nextElementSibling.classList.toggle("visible");
+    e.target.nextElementSibling.style.maxHeight = 0;
+    return;
+  }
+
+  if (e.target.classList.contains("balance__caption") && !e.target.classList.contains("active")) {
+    e.target.classList.toggle("active");
+    e.target.nextElementSibling.classList.toggle("visible");
+    e.target.nextElementSibling.style.maxHeight = e.target.nextElementSibling.scrollHeight + "px";
+    return;
+  }
+});
