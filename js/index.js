b5e02f3fbbc280ec5d1e3ae54205c463f2965c2b
Askarunro <paskar.y.a@gmail.com>
2022-08-09 16:19:15 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,50 @@
 const refs = {
   balanceList: document.querySelector(".balance__list"),
+  balanceItems: document.querySelectorAll(".balance__item"),
   garnomyImgWrapper: document.querySelector(".garnomy__img--wrapper"),
   garnomyInfoWrapper: document.querySelector(".garmony__info--wrapper"),
+  balanceCaptions: document.querySelectorAll(".balance__caption"),
 };
-refs.balanceList.addEventListener("click", (e) => {
-  if (e.target.classList.contains("balance__caption") && e.target.classList.contains("active")) {
-    e.target.classList.toggle("active");
-    e.target.nextElementSibling.classList.toggle("visible");
-    e.target.nextElementSibling.style.maxHeight = 0;
-    return;
-  }
+// refs.balanceList.addEventListener("click", (e) => {
+//   // клік закриття
+//   if (e.target.classList.contains("balance__caption") && e.target.classList.contains("active")) {
+//     e.target.classList.toggle("active");
+//     e.target.nextElementSibling.classList.toggle("visible");
+//     e.target.nextElementSibling.style.maxHeight = 0;
+//     return;
+//   }
+//   // клік відкриття
+//   if (e.target.classList.contains("balance__caption") && !e.target.classList.contains("active")) {
+//     e.target.classList.toggle("active");
+//     e.target.nextElementSibling.classList.toggle("visible");
+//     e.target.nextElementSibling.style.maxHeight = e.target.nextElementSibling.scrollHeight + "px";
+//     return;
+//   }
+// });
 
-  if (e.target.classList.contains("balance__caption") && !e.target.classList.contains("active")) {
-    e.target.classList.toggle("active");
-    e.target.nextElementSibling.classList.toggle("visible");
-    e.target.nextElementSibling.style.maxHeight = e.target.nextElementSibling.scrollHeight + "px";
-    return;
-  }
+refs.balanceCaptions.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+      item.nextElementSibling.classList.remove("visible");
+      item.nextElementSibling.style.maxHeight = 0;
+      return;
+    }
+    if (!item.classList.contains("active")) {
+      refs.balanceItems.forEach((elem) => {
+        if (elem.querySelector(".balance__caption.active")) {
+          elem.querySelector(".balance__caption.active").classList.remove("active");
+          elem.querySelector(".balance__item--inner.visible").style.maxHeight = 0;
+          elem.querySelector(".balance__item--inner.visible").classList.remove("visible");
+
+          return;
+        }
+      });
+      item.classList.toggle("active");
+      item.nextElementSibling.classList.toggle("visible");
+      item.nextElementSibling.style.maxHeight = item.nextElementSibling.scrollHeight + "px";
+    }
+  });
 });
 
 // const garmonyVisible = () => {
