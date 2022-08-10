5d8eb2bf163ca795902891141e515a85826d0785
Askarunro <paskar.y.a@gmail.com>
2022-08-10 10:35:57 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,22 +6,6 @@ const refs = {
   balanceCaptions: document.querySelectorAll(".balance__caption"),
   garmonyLink: document.querySelector(".garmony__link"),
 };
-// refs.balanceList.addEventListener("click", (e) => {
-//   // клік закриття
-//   if (e.target.classList.contains("balance__caption") && e.target.classList.contains("active")) {
-//     e.target.classList.toggle("active");
-//     e.target.nextElementSibling.classList.toggle("visible");
-//     e.target.nextElementSibling.style.maxHeight = 0;
-//     return;
-//   }
-//   // клік відкриття
-//   if (e.target.classList.contains("balance__caption") && !e.target.classList.contains("active")) {
-//     e.target.classList.toggle("active");
-//     e.target.nextElementSibling.classList.toggle("visible");
-//     e.target.nextElementSibling.style.maxHeight = e.target.nextElementSibling.scrollHeight + "px";
-//     return;
-//   }
-// });
 
 refs.balanceCaptions.forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -37,7 +21,6 @@ refs.balanceCaptions.forEach((item) => {
           elem.querySelector(".balance__caption.active").classList.remove("active");
           elem.querySelector(".balance__item--inner.visible").style.maxHeight = 0;
           elem.querySelector(".balance__item--inner.visible").classList.remove("visible");
-
           return;
         }
       });
@@ -46,30 +29,7 @@ refs.balanceCaptions.forEach((item) => {
       item.nextElementSibling.style.maxHeight = item.nextElementSibling.scrollHeight + "px";
     }
   });
-  // item.addEventListener("oninput ", (e) => {
-  //   console.log(item.querySelector("input"));
-  // });
 });
-
-// refs.balanceItems.forEach((item) => {
-//   const input = item.querySelector(".rangeslider__value-bubble");
-//   console.log(input);
-//   input.addEventListener(
-//     "DOMCharacterDataModified",
-//     function (event) {
-//       console.log(hi);
-//     },
-//     false
-//   );
-// });
-
-// refs.balanceItems.forEach((item) => {
-//   const input = item.querySelector("input");
-//   console.log(input.value);
-//   input.addEventListener("input", (e) => {
-//     console.log("hi");
-//   });
-// });
 
 const garmonyVisible = () => {
   console.log(refs.garnomyImgWrapper.offsetHeight);
